a03cb561b866afdb63ae5e58eb95bf5316bf8eb5
Anurag <aknrg627@gmail.com>
2021-05-13 23:18:43 +0530

    production ready added env variables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const routes = require('./routes/index');
